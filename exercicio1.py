38cbcfb4871f6280d5d5bd2f97e83b432d30e249
Juliano Laurentino <66442729+JulianoLaurentino@users.noreply.github.com>
2021-01-17 18:53:02 +0000

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -6,30 +6,30 @@
 
 A = float(input("Primeiro Valor: "))
 B = float(input("Segundo Valor: "))
-C = float(("Terceiro Valor: "))
+C = float(input("Terceiro Valor: "))
 
 x = 0
 
 if A < B:
     x = A
     A = B 
-    B = X
+    B = x
     
 if B < A:
     x = B
     B = A
-    A = X
+    A = x
     
 if C < B:
     x = C
     C = B 
-    C = X
+    C = x
 
 if (A > B+C):
     print("NAO FORMA TRIANGULO")
 elif (A**2 == B**2 + C**2):
     print("TRIANGULO RETANGULO")
-elif (A**2 > b**2 + c**2):
+elif (A**2 > B**2 + C**2):
     print("TRIANGULO OBUSANGULO")
 elif (A**2 < B**2 + C**2):
     print("TRIANGULO ACUTANGULO")
