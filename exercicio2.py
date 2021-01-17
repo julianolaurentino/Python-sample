ab5b054cb9ea9d5c78bb1515ec3169033e75662c
Juliano Laurentino <julianolaurentino@gmail.com>
2021-01-17 16:59:58 +0000

    Add file exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,23 @@
+# _*_ coding: utf-8 _*_
+
+# Criando um codigo para informar na tela o dia 
+# da semana correspondente ao numero solicitado
+
+
+a = int(input("Digite aqui um numero de 1 a 7: "))
+if a == 1:
+    print("1 = Segunda")
+elif a == 2:
+    print("2 = Terca")
+elif a == 3:
+    print("3 = Quarta")
+elif a == 4:
+    print("4 = Quinta")
+elif a == 5:
+    print("5 = Sexta")
+elif a == 6:
+    print("6 = Sabado")
+elif a == 7:
+    print("7 = Domingo")
+print("Fim do Programa")
+
