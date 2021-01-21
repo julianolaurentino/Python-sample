ff2dab8ca508c5efc032e60c566d5be0109defcf
unknown <julianolaurentino@gmail.com>
2021-01-21 11:28:11 -0300

    add file exercicio5.py

---
+++ exercicio5.py
@@ -0,0 +1,24 @@
+# _*_ coding: utf-8 _*_
+
+# Criando uma calculadora
+
+a = int(input("Digite o Primeiro número aqui: "))
+b = int(input("Digite o Segundo número aqui: "))
+ 
+soma = a + b
+subtracao = a - b 
+multiplicacao = a * b 
+divisao = a / b 
+resto = a % b 
+
+Resultado = ('Soma: {soma}'
+            '\nSubtração: {subtracao}'
+            '\nMultiplicação: {multiplicacao}'
+            '\nDivisão: {divisao}'
+            '\nResto: {resto}'.format(soma=soma,
+                                    subtracao=subtracao,
+                                    multplicacao=multiplicacao,
+                                    divisao=divisao,
+                                    resto=resto,))
+print(Resultado)
+ 
