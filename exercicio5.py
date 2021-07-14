1b9a395e432b22e87263402ca96bca8ee612ec58
Juliano Laurentino <66442729+JulianoLaurentino@users.noreply.github.com>
2021-07-14 17:26:41 -0300

    Update exercicio5.py

--- exercicio5.py
+++ exercicio5.py
@@ -4,21 +4,12 @@
 
 a = int(input("Digite o Primeiro número aqui: "))
 b = int(input("Digite o Segundo número aqui: "))
- 
-soma = a + b
-subtracao = a - b 
-multiplicacao = a * b 
-divisao = a / b 
-resto = a % b 
 
-resultado = ('Soma: {soma}'
-            '\nSubtração: {subtracao}'
-            '\nMultiplicação: {multiplicacao}'
-            '\nDivisão: {divisao}'
-            '\nResto: {resto}'.format(soma=soma,
-                                    subtracao=subtracao,
-                                    multplicacao=multiplicacao,
-                                    divisao=divisao,
-                                    resto=resto,))
-print(resultado)
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+print('\n''Resultado de sua calculadora: ')
+print(f'Soma:{soma}''\n'f'Subtração:{subtracao}''\n'f'Multiplicação:{multiplicacao}''\n'f'Divisão:{divisao}''\n'f'Resto:{resto}')
  
