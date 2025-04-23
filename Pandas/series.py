7a82573f00221680c8d8ea6f0e2f853f562365ca
Juliano Laurentino <julianolaurentino@gmail.com>
2025-04-22 22:55:16 -0300

    new files pandas

---
+++ Pandas/series.py
@@ -0,0 +1,38 @@
+# %%
+import pandas as pd
+
+idades = [
+    32, 38, 30, 30, 31,
+    35, 25, 29, 31, 37,
+    27, 23, 36, 33, 32,
+]
+
+media = sum(idades) / len(idades)
+print("Media:",media) 
+
+diffs = 0
+for i in idades:
+    diffs += (i - media) ** 2
+variancia = diffs / len(idades)-1
+print("Variancia:",variancia) 
+
+# %%
+import pandas as pd
+
+idades = [
+    32, 38, 30, 30, 31,
+    35, 25, 29, 31, 37,
+    27, 23, 36, 33, 32,
+]
+
+series_idades = pd.Series(idades)
+
+media_idades = series_idades.mean()
+print("media:" ,media_idades)
+
+variancia_idades = series_idades.var()
+print("variancia:" ,variancia_idades)
+
+sumary_idades = series_idades.describe()
+print("sumary:" ,sumary_idades)
+
---
+++ examples/adicionando-numeros-while.py
@@ -0,0 +1,6 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+df = spark.sql("select 'spark' as hello ")
+
+df.show()
