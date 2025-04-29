21bfcc0bbaccb724d9962877337a829734030384
Juliano Laurentino <julianolaurentino@gmail.com>
2025-04-28 23:10:17 -0300

    data: add novas amostras

--- Pandas/filtros.py
+++ Pandas/filtros.py
@@ -23,5 +23,23 @@ brinquedo = pd.DataFrame(
         "uf": ["sp","rj","ce"],
     }
 )
-filtro = brinquedo["idade"] > 30
-brinquedo[["uf","nome"]]
+filtro = brinquedo["idade"] == 30
+brinquedo[filtro]
+
+# %%
+#qtd de pontos acima de 50
+
+df = pd.read_csv('../data/transacoes.csv')
+df.head(5)
+
+pontos_acima_50 = df['qtdePontos'] >= 50
+df[pontos_acima_50]
+# %%
+#qtd de pontos acima de 50 e menor que 100
+acima_50_abaixo_100 = (df['qtdePontos'] >= 50) * (df['qtdePontos'] < 100)
+df[acima_50_abaixo_100]
+
+# %%
+#utilizando o "ou" |
+acima_50_ou_abaixo_100 = (df['qtdePontos'] == 50) | (df['qtdePontos'] == 100)
+df[acima_50_ou_abaixo_100]
