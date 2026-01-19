6c5a86343e7e7b6188dde92e38863201df527008
Juliano Laurentino <66442729+julianolaurentino@users.noreply.github.com>
2026-01-19 20:32:32 -0300

    data: novo arquivos consulta-presidents

---
+++ exercicios/conuslta-presidents.py
@@ -0,0 +1,13 @@
+# %%
+import pandas as pd
+import requests
+# %%
+# Exemplo: Tratandop dados retornados pela API
+url = 'https://api.sampleapis.com/presidents/presidents'
+df = pd.DataFrame(requests.get(url).json())
+#df[['inicio','fim']] = df.yearsInOffice.str.split(' - ', expand=True)
+df
+
+# %%
+df_vice = df.explode('vicePresidents')[['name','vicePresidents']]
+df_vice
