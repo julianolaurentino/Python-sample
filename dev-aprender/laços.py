032a994c2e2dd28a0b5fc1136198a4a739e3fd3a
Juliano Laurentino <66442729+julianolaurentino@users.noreply.github.com>
2026-01-24 10:55:06 -0300

    data: adicionando laços

---
+++ dev-aprender/laços.py
@@ -0,0 +1,26 @@
+# laços de repetição
+
+for item in range(3):
+    print('item')
+
+'''
+for item in colecao:
+    comandos
+'''
+
+# Range nunca inclui o ultimo numero
+
+for item in range(10):
+    print(item)
+
+# Definindo um valor inicial
+
+for item in range(1,10):
+    print(item)
+
+# Definindo passo
+
+for item in range(1,10,2):
+    print(item)
+
+
