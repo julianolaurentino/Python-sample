5fc2453f8bb02ecd45074f9da0490bf82afed436
julianolaurentino <julianolaurentino@gmail.com>
2026-01-27 20:05:11 -0300

    data: acrescentando desafio

--- dev-aprender/listas.py
+++ dev-aprender/listas.py
@@ -10,3 +10,11 @@ salarios = [2500, 5000, 7000]
 salario_usuario = float(input('Digite o seu salário: '))
 salarios.append(salario_usuario)
 print(f'Salários atualizados: {salarios}')
+
+
+# Desafio
+# gastos totais com pagamento de salários
+# dado uma lista de salários, calcule o total pago a todos os funcionários
+salarios_funcionarios = [1300, 2000, 3400, 5000]
+total_gastos = sum(salarios_funcionarios)
+print(f'Total gasto com salários: R$ {total_gastos}')
