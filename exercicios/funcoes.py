75c7b324ad9d8847624b25c14cceb404f4cca4d7
julianolaurentino <julianolaurentino@gmail.com>
2026-02-01 20:16:55 -0300

    data: adicionando novos exemplos de funções

--- exercicios/funcoes.py
+++ exercicios/funcoes.py
@@ -11,7 +11,8 @@ minha_funcao()
 print(nome)
 '''
 
-# funções globais
+
+# --funções globais--
 # exemplo colocando a variável nome conmpleto fora da função e tentando chamar dentro da função 
 # funciona por ser global
 nome = 'juliano'
@@ -19,3 +20,28 @@ nome = 'juliano'
 def minha_funcao():
     print(f'Este é o meu nome: {nome}')
 minha_funcao()
+
+
+# --Parametros e argumentos--
+# parametros é o que a função vai receber:
+def somar(n1, n2):
+    resultado = n1 + n2
+    print(f'A soma entre {n1} e {n2} é igual a {resultado}')
+
+# argumentos são os valores que são enviados para os parametros
+somar(5,4)
+
+# exemplo com string
+def niveis(nivel):
+    print(f'O seu nivel em python é: {nivel}')
+niveis('Pleno')
+
+
+# Fazer com que algum resultado retorne algum valor para a variável
+def subtrair(n1, n2):
+    resultado = n1 - n2
+    return resultado
+# o return aplica o valor dela para a variável
+resultado_subtrair = subtrair(2,1)
+print(resultado_subtrair)
+
