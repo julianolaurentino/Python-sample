c55a0205b6cf8521c394a00bf3fe2054abc94655
julianolaurentino <julianolaurentino@gmail.com>
2026-02-12 20:49:31 -0300

    data: adicionando exemplo de função da hashtag treinamentos

---
+++ exercicios/funcoes_hashtag.py
@@ -0,0 +1,20 @@
+# Função -> um bloco de código reutilizável e organizacional
+
+# Sintaxe 
+'''
+def nome_da_funcao(parametros):
+    instrucao 1
+    instrucao 2
+    ...
+    return valor (opcionaal)
+'''
+
+# exemplo
+def calcular_imposto(valor):
+    if valor < 1000:
+        imposto = valor * 0.1
+    elif valor < 2000:
+        imposto = valor * 0.13
+    else:
+        imposto = valor * 0.2
+    return imposto
