6ea1bb2ba5d87ea7ea95b7ff9b8db2ce12759112
julianolaurentino <julianolaurentino@gmail.com>
2026-02-17 18:22:28 -0300

    data: adicionando exercicio de metodos

--- dev-aprender/classe_metodo.py
+++ dev-aprender/classe_metodo.py
@@ -18,6 +18,7 @@ class computador:
         self.placa_video = 'nvidia'
     pass
 
+# instancias
 computador1 = computador()
 # print(computador1.marca)
 computador2 = computador()
@@ -32,9 +33,32 @@ class computador:
         self.memoria_ram = memoria_ram
         self.placa_video = placa_video    
     pass
-computador1 = computador('asus','4gb','nvidea')
+
+# instancias
+computador1 = computador('asus','4gb','nvidia')
 computador2 = computador('positivo','12gb','geforce')
 computador3 = computador('dell','8gb','atm')
 print(computador1.marca, computador1.memoria_ram, computador1.placa_video)
 print(computador2.marca, computador2.memoria_ram, computador2.placa_video)
 print(computador3.marca, computador3.memoria_ram, computador3.placa_video)
+
+# Criando metodos
+class computador:
+    def __init__(self, marca, memoria_ram, placa_video):
+        self.marca = marca
+        self.memoria_ram = memoria_ram
+        self.placa_video = placa_video    
+    def ligar(self):
+        print('Ligando o computador')
+
+    def desligar(self):
+        print('Desligando o computador')
+
+    def exibir_informacoes(self):
+        print(self.marca, self.memoria_ram, self.placa_video)
+
+# instancias
+computador1 = computador('asus','4gb','nvidia')
+computador1.ligar()
+computador1.desligar()
+computador1.exibir_informacoes()
