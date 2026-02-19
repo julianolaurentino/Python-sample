a9de1383871bba877a5d5e254f6ed37823fb705a
julianolaurentino <julianolaurentino@gmail.com>
2026-02-19 20:48:59 -0300

    data: teste com zed ide

--- exercicios_pt2/nivel_1.py
+++ exercicios_pt2/nivel_1.py
@@ -10,4 +10,3 @@ segundo_numero = int(input("Digite o segundo numero: "))
 soma = primeiro_numero + segundo_numero
 
 print(f"A soma de {primeiro_numero} com {segundo_numero} é {soma}")
-print("Teste com zed ide")
