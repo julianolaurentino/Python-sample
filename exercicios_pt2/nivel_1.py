335983ed1aae5f938f92fe80bfeffdc0f01aaaad
julianolaurentino <julianolaurentino@gmail.com>
2026-02-19 20:11:08 -0300

    data: teste realizado no zed ide

---
+++ exercicios_pt2/nivel_1.py
@@ -0,0 +1,12 @@
+"""
+Exercicios para fixação nivel basico
+Laços, variáveis e funções
+"""
+
+# Crie um programa de soma simples
+print("Calculadora simples")
+primeiro_numero = int(input("Digite o primeiro numero: "))
+segundo_numero = int(input("Digite o segundo numero: "))
+soma = primeiro_numero + segundo_numero
+
+print(f"A soma de {primeiro_numero} com {segundo_numero} é {soma}")
