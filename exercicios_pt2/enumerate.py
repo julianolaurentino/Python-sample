7523946cda4ab805d9b0c3c6176e134c4c9ee84a
julianolaurentino <julianolaurentino@gmail.com>
2026-03-22 19:40:07 -0300

    data: add enumerate.py e sistema_log.py

---
+++ exercicios_pt2/enumerate.py
@@ -0,0 +1,8 @@
+frutas = ["maçã", "banana", "laranja"]
+for indice, fruta in enumerate(frutas):
+    print(f"{indice}: {fruta}")
+
+
+tasks = ["task1", "task2", "task3"]
+for index, task in enumerate(tasks):
+    print(f"{index}: {task}")
