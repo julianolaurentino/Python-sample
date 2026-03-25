4f41555634a80e5388f30905065407ae496661b5
julianolaurentino <julianolaurentino@gmail.com>
2026-03-25 20:25:19 -0300

    data: add conversao_tempo_contrato

---
+++ hashtag_exercicios/variaveis/conversao_tempo_contrato.py
@@ -0,0 +1,12 @@
+"""
+Exercício 5: Conversão de Tempo de Contrato (Gestão de Projetos)
+Cenário: Um contrato de manutenção de software tem a duração de 40 meses. O cliente
+quer ver esse tempo no formato: "X anos e Y meses". Objetivo: Utilize os operadores de
+divisão inteira e resto da divisão para converter os 40 meses.
+"""
+
+contrato = 40
+anos = contrato // 12
+meses = contrato % 12
+
+print(f"{anos} anos e {meses} meses")
