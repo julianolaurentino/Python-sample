549aea510953a27f4d625ce6193208ca01c37f04
julianolaurentino <julianolaurentino@gmail.com>
2026-03-28 13:14:01 -0300

    data: acrescentando contador de gap liso liso liso

---
+++ exercicios_pt2/contador_gap.py
@@ -0,0 +1,52 @@
+# contador de gap
+
+
+gap = 0
+print("=" * 60)
+print(f"Quanto de gap ja tomei ate o momento: {gap}")
+while True:
+    try:
+        gap += int(input(f"Acrescentar gap: "))
+        break
+    except ValueError:
+        print("Entrada inválida. Digite um número inteiro: ")
+print("=" * 60)
+print(f"Gap atual: {gap}")
+print(f'Opções: "s" para continuar, "n" para sair, "r" para resetar.')
+print(f"Deseja continuar? (s/n/r)")
+opcao = input()
+while opcao != "s" and opcao != "n" and opcao != "r":
+    print(
+        "Opção inválida. Digite 's' para continuar ou 'n' para sair ou 'r' para resetar."
+    )
+    opcao = input()
+if opcao == "s":
+    while True:
+        try:
+            gap += int(input(f"Acrescentar gap: "))
+            print(f"Gap atual: {gap}")
+            print(f"deseja continuar? (s/n/r)")
+            opcao = input()
+            while opcao != "s" and opcao != "n" and opcao != "r":
+                print(
+                    "Opção inválida. Digite 's' para continuar ou 'n' para sair ou 'r' para resetar."
+                )
+                opcao = input()
+            if opcao == "n":
+                print(f"Gap final: {gap}")
+                print("Encerrando o programa.")
+                break
+            elif opcao == "r":
+                gap = 0
+                print("Gap resetado para 0.")
+                break
+            continue
+        except ValueError:
+            print("Entrada inválida. Digite um número inteiro: ")
+elif opcao == "n":
+    print(f"Gap final: {gap}")
+    print("Encerrando o programa.")
+elif opcao == "r":
+    gap = 0
+    print("Gap resetado para 0.")
+print("=" * 60)
