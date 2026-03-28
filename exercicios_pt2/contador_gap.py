eab4b221e3b60ae1e548083fe9449a05ab867f9b
julianolaurentino <julianolaurentino@gmail.com>
2026-03-28 14:16:19 -0300

    fix: ajustando alguns prints

--- exercicios_pt2/contador_gap.py
+++ exercicios_pt2/contador_gap.py
@@ -1,19 +1,18 @@
-# contador de gap
-
+# contador de gap baseado no conteudo do youtuber de extremo sucesso.
 
 gap = 0
 print("=" * 60)
 print(f"Quanto de gap ja tomei ate o momento: {gap}")
 while True:
     try:
-        gap += int(input(f"Acrescentar gap: "))
+        gap += int(input("Acrescentar gap: "))
         break
     except ValueError:
         print("Entrada inválida. Digite um número inteiro: ")
 print("=" * 60)
 print(f"Gap atual: {gap}")
-print(f'Opções: "s" para continuar, "n" para sair, "r" para resetar.')
-print(f"Deseja continuar? (s/n/r)")
+print('Opções: "s" para continuar, "n" para sair, "r" para resetar.')
+print("Deseja continuar? (s/n/r)")
 opcao = input()
 while opcao != "s" and opcao != "n" and opcao != "r":
     print(
@@ -23,9 +22,9 @@ while opcao != "s" and opcao != "n" and opcao != "r":
 if opcao == "s":
     while True:
         try:
-            gap += int(input(f"Acrescentar gap: "))
-            print(f"Gap atual: {gap}")
-            print(f"deseja continuar? (s/n/r)")
+            gap += int(input("Acrescentar gap: "))
+            print(f"gap atual: {gap}")
+            print("Deseja continuar? (s/n/r)")
             opcao = input()
             while opcao != "s" and opcao != "n" and opcao != "r":
                 print(
