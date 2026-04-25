92b2c2e98d71c8d9e21af66569b80762079472d5
julianolaurentino <julianolaurentino@gmail.com>
2026-04-25 14:13:46 -0300

    data: adicionando novas linhas de codigo

--- exercicios_pt2/poo.py
+++ exercicios_pt2/poo.py
@@ -1,4 +1,6 @@
 # exemplo de classe carro
+
+
 class Carro:
     def __init__(self, marca, modelo, ano):
         self.marca = marca
@@ -17,7 +19,7 @@ class CarroPopular(Carro):
         self.preco = preco
 
     def exibir_informacoes(self):
-        # herança do método
+        # herança do método exibir_informacoes
         super().exibir_informacoes()
         print(f"Preço: {self.preco}")
 
@@ -26,3 +28,7 @@ mustang = Carro("Ford", "Mustang", 2022)
 mustang.exibir_informacoes()
 celta = CarroPopular("Chevrolet", "Celta", 2010, 20000)
 celta.exibir_informacoes()
+gol = CarroPopular("Volkswagen", "Gol", 2015, 30000)
+gol.exibir_informacoes()
+astra = CarroPopular("Opel", "Astra", 2018, 40000)
+astra.exibir_informacoes()
