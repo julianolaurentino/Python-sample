92a6694d08f79c2233a7f7f5437dcc9ba46d4c1b
julianolaurentino <julianolaurentino@gmail.com>
2026-04-29 17:59:32 -0300

    data: testando novas funcionalidades zed ide

--- dev-aprender/chutar_numeros.py
+++ dev-aprender/chutar_numeros.py
@@ -6,36 +6,37 @@
 # o programa deve informar se o chute foi maior, menor ou igual ao valor aleatorio gerado no inicio
 
 import random
+
 numero_random = random.randint(1, 10)
-numero_usuario = int(input('Chute um numero de 1 a 10: '))
+numero_usuario = int(input("Chute um numero de 1 a 10: "))
 while True:
     if numero_usuario > numero_random:
-        print('Errou! Seu numero é maior.')
-        numero_usuario = int(input('Chute novamente: '))
+        print("Errou! Seu numero é maior.")
+        numero_usuario = int(input("Chute novamente: "))
     elif numero_usuario < numero_random:
-        print('Errou! Seu numero é menor.')
-        numero_usuario = int(input('Chute novamente: '))
-    else:    
-        print('Parabens! Voce acertou!')    
+        print("Errou! Seu numero é menor.")
+        numero_usuario = int(input("Chute novamente: "))
+    else:
+        print("Parabens! Voce acertou!")
         break
 
 
-# Solução com contador de palpites 
+# Solução com contador de palpites
 
 from random import randint
+
 computador = randint(0, 10)
-print('Adivinhe o número que estou pensando ')
+print("Adivinhe o número que estou pensando ")
 acertou = False
 palpite = 0
 while not acertou:
-    jogador = int(input('Qual é o seu palpite? '))
+    jogador = int(input("Qual é o seu palpite? "))
     palpite += 1
     if jogador == computador:
         acertou = True
     else:
         if jogador < computador:
-            print('Mais...Tente mais um vez.')
+            print("Mais...Tente mais um vez.")
         elif jogador > computador:
-            print('Menos...Tente mais uma vez.')
-print(f'Acertou com {palpite} tentativas. Parabéns!')
-
+            print("Menos...Tente mais uma vez.")
+print(f"Acertou com {palpite} tentativas. Parabéns!")
--- exercicios_pt2/teste.py
+++ exercicios_pt2/teste.py
@@ -1,16 +1,11 @@
 # %%
 import pandas as pd
-import requests
+import pyarrow.parquet as pq
 
-url = "https://api.sampleapis.com/presidents/presidents"
-df = pd.DataFrame(requests.get(url).json())
-# df[['inicio','fim']] = df.yearsInOffice.str.split(' - ', expand=True)
-df
+base = "../../dataset/anac_combinada/parquet/combinada2025-01.parquet"
+df = pd.read_parquet(base)
 
-# %%
-
-base = "../../dataset/netflix_titles.csv"
-df_netflix = pd.read_csv(base)
-df_netflix.head()
-
+# mostar todas as colunas no dataframe
+pd.set_option("display.max_columns", None)
+df.head()
 # %%
